3909ee9999b0b8257c673e4eff13a3d6f0ff2317
sangyongchoi <csy7792@naver.com>
2021-05-04 23:56:50 +0900

    사용하지 않는 의존성 및 package 제거

--- board/build.gradle.kts
+++ board/build.gradle.kts
@@ -23,7 +23,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
-    implementation("org.springframework.data:spring-data-r2dbc")
     implementation("io.r2dbc:r2dbc-h2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
--- board/src/main/kotlin/com/webflux/board/BoardApplication.kt
+++ board/src/main/kotlin/com/webflux/board/BoardApplication.kt
@@ -3,10 +3,8 @@ package com.webflux.board
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
 @SpringBootApplication
-@EnableR2dbcRepositories(basePackageClasses = [BoardApplication::class])
 @EnableReactiveMongoRepositories(basePackageClasses = [BoardApplication::class])
 class BoardApplication
 
